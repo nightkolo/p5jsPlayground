7655f8eacc86382516ffafab8d86c1d8e86b560b
Kolo <thenitezune104@gmail.com>
2025-03-26 22:30:55 +0400

    blow til ya pop

--- README.md
+++ README.md
@@ -6,20 +6,21 @@ A dumping hodgepodge of my random coding project I do for practice!
 
 These are largely made for fun and consequently may not possess the soundest quality.
 
-**I will also do no maintence whatsoever!**
+**I will also do no maintenance whatsoever!**
 
 ## How do I use this?
 
-For now, you can clone the repo, or simply download by going to `Code > Download ZIP`, run a directory on a browser. [Everything's modifiable too](LICENSE).
+For now, you can clone the repo, or simply download by clicking to **Code** > **Download ZIP**, and open the `index.html` in a directory to run a project, and `sketch.js` to view the code. [Everything's modifiable too](LICENSE).
 
 The Github Pages deployment would be coming soon.
 
 ## Interesting ones
 | Link      | Devlog     |
 | ------------- | ------------- |
+| [Blow 'Till Pop](blow-til-pop/) | An endless game where you blow bubbles and avoid bursting them. Simple on surface, but quite interesting to code, probably the most fun I've had coding with Geometry. |
 | [Bouncing Ball](bouncing-ball/) | I coded this as a simple and sweet visual, inspired by Windows' solitare win screen. |
 | [Circlepong](circlepong/) | This is a highly primitive game I coded inspired by Pong. |
 | [Flappy Circle (WIP)](flappy-circle/) | A game inspired by [Don't Touch The Spikes](https://youtu.be/7t3qfK6mP64). Though I initially coded it an a flappy bird clone, but I thought this direction would be more interesting to develop |
 | [Houses for Sale (WIP)](house-for-sale/) | Randomly generated houses. |
-| [Snake (WIP)](snake/) | The snake's tail and collision were a bit tricky though, I decided it to keep it as a wip for the time being. It was fun making this. |
+| [Snake (WIP)](snake/) | The snake's tail and collision were a bit tricky though, I decided it to keep it as a work in progress for the time being. It was fun making this. |
 | [Legacy](legacy/) | Contains my older coding projects. |
--- blow-til-pop/sketch.js
+++ blow-til-pop/sketch.js
@@ -1,7 +1,125 @@
-function setup() {
-  createCanvas(400, 400);
-}
+var canSize = 600;
+var bubbleIsBlown = false;
+var bubbles = [];
 
+let currentBub;
+let bubsScores;
+
+class Bubble {
+  // What does it MEAN to be a bubble?!?!?!
+  constructor(x = 0, y = 0, size = 0, blow = true) {
+    this.isBlown = blow;
+    this.bubX = x
+    this.bubY = y
+    this.bubSpeedX = 0;
+    this.bubSpeedY = 0;
+    this.bubInc = 0.8;
+    this.bubSize = size;
+    this.dir = {
+      x: Math.sign(Math.random()-0.5), y: Math.sign(Math.random()-0.5)
+    };
+    this.speedFactor = 90;
+    this.isAlive = true;
+  }
+  burst() {
+    console.log("I'm hitting somthing!");
+    this.isAlive = false;
+  }
+  getScore() {
+    return Math.round(this.bubSize / 10);
+  }
+  update(){
+    if (this.isAlive) {
+      this.bubSpeedX = this.speedFactor/this.bubSize;
+      this.bubSpeedY = (this.speedFactor-10)/this.bubSize;
+  
+      if (this.isBlown) {
+        this.bubSize += this.bubInc;
+        this.bubX = mouseX;
+        this.bubY = mouseY;
+      } else {
+        if (this.bubX > width - (this.bubSize/2) || this.bubX < this.bubSize/2){
+          this.dir.x = -1 * this.dir.x;
+        }
+        if (this.bubY > height - (this.bubSize/2) || this.bubY < this.bubSize/2){
+          this.dir.y = -1 * this.dir.y;
+        }
+
+        this.bubX += this.dir.x * this.bubSpeedX;
+        this.bubY += this.dir.y * this.bubSpeedY;
+      }
+    }
+  }
+  show(){
+    if (this.isAlive) {
+      circle(this.bubX, this.bubY, this.bubSize);
+      textAlign(CENTER);
+      textSize(5 + (this.bubSize/4));
+      text(`${this.getScore()}`, this.bubX, this.bubY+1);
+    }
+  }
+}
+function getBubsScores(bubs){
+  let res = 0;
+  bubs.forEach(function(b) {
+    res += b.getScore();
+  })
+  return res;
+}
+function bubbleHasBursted(bursted_bub){
+  bubbles = bubbles.filter(bubble => bubble !== bursted_bub);
+}
+function setup() {
+  createCanvas(canSize, canSize);
+  frameRate(60);
+  firstBubble = new Bubble(width/2, height/2, 30, false);
+  bubbles.push(firstBubble)
+}
 function draw() {
   background(220);
+  
+  bubbles.forEach(function(bub) {
+    bub.update();
+    bub.show();
+  })
+
+  if (bubbleIsBlown){
+    bubbles.forEach(function(bub){
+      if (currentBub !== bub) {
+        if (borderCollision(currentBub.bubSize, currentBub.bubX, currentBub.bubY)) {
+          bubbleHasBursted(currentBub);
+          currentBub.burst();
+        }
+        if (circleCollision(currentBub.bubX, currentBub.bubY, currentBub.bubSize, bub.bubX, bub.bubY, bub.bubSize)) {
+          bubbleHasBursted(currentBub);
+          currentBub.burst();
+        }
+      }
+    })
+  }
+}
+function circleCollision(x1, y1, d1, x2, y2, d2) {
+  let distance = dist(x1, y1, x2, y2);
+  return distance <= (d1 / 2) + (d2 / 2);
+}
+function borderCollision(bubSize, bubX, bubY) {
+  return (
+    bubX - (bubSize / 2) <= 0 ||  // Left border
+    bubX + (bubSize / 2) >= width ||  // Right border
+    bubY - (bubSize / 2) <= 0 ||  // Top border
+    bubY + (bubSize / 2) >= height  // Bottom border
+  );
+}
+function mousePressed(){
+  bubbleIsBlown = true;
+  var newBubble = new Bubble();
+  currentBub = newBubble;  
+  bubbles.push(newBubble);
+}
+function mouseReleased(){
+  bubbleIsBlown = false;
+  currentBub.isBlown = false;
+  bubsScores = getBubsScores(bubbles);
+  console.log(bubsScores);
+  console.log(bubbles);
 }
