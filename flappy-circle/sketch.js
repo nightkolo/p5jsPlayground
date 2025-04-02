25d23970914ed8143f163bfcce4c99ddd32c8c97
Kolo <thenitezune104@gmail.com>
2025-04-02 16:15:30 +0400

    Updated flappy-circle
    Moving spikes prototype

--- flappy-circle/sketch.js
+++ flappy-circle/sketch.js
@@ -12,14 +12,51 @@ const spikesEas = new Array(
   [0, 0, 0, 1, 1, 0, 0, 1],
   [0, 1, 0, 0, 1, 0, 0, 1],
 );
-const spikesMed = new Array();
-const spikesHar = new Array();
+const spikesMed = new Array(
+  [1, 2, 0, 2, 0, 2, 0, 0]  
+  // [0, 1, 0, 1, 0, 0, 1, 0],
+  // [1, 0, 1, 1, 1, 0, 0, 1],
+  // [0, 1, 1, 0, 1, 0, 0, 1],
+  // [1, 0, 0, 1, 0, 1, 1, 0],
+  // [1, 0, 0, 1, 1, 0, 0, 1],
+  // [0, 0, 1, 1, 1, 1, 0, 0],
+  // [1, 1, 1, 0, 0, 1, 1, 1],
+  // [1, 0, 0, 1, 0, 0, 1, 0],
+  // [1, 1, 0, 1, 0, 1, 0, 0]
+);
+const spikesHar = new Array(
+  [1, 0, 1, 0, 1, 0, 1, 0],
+  [1, 1, 1, 0, 1, 1, 1, 1],
+  [0, 1, 1, 0, 1, 1, 0, 1],
+  [1, 1, 1, 1, 1, 1, 0, 1],
+  [0, 1, 1, 0, 1, 1, 1, 0],
+  [1, 1, 0, 1, 1, 1, 0, 1],
+  [0, 1, 1, 0, 1, 0, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 0, 0, 0, 0, 0]
+);
+let movingSpikes = [];
 
 var score = 0;
-var t = 0;
+var t = 14;
+var difficulty = 1;
 
 let p;
 let spikeLength = 0;
+
+const getCurrentSpikes = function(){
+  switch (difficulty){
+    case 1:
+      return spikesEas;
+      break;
+    case 2:
+      return spikesMed;
+      break;
+    case 3:
+      return spikesHar;
+      break;
+  }
+}
 
 class Player {
   constructor() {
@@ -66,6 +103,19 @@ class Player {
     this.velY = this.jumpHeight;
   }
 }
+class MovingSpike {
+  constructor(pY, pX){
+    this.y = pY;
+    this.x = pX;
+  }
+  update(){
+    this.y += 0.2;
+  }
+  show(){
+    rectMode(CENTER);
+    rect(this.x, this.y, spikeSize/2, spikeSize); 
+  }
+}
 function setSpikes() {
   spikeLength = height/spikeSize;
   // for (let i = 0; i < spikeLength; i += 1) {
@@ -73,48 +123,97 @@ function setSpikes() {
   // }
   newSpikes();
 }
+
+var areMovingSpikes = false;
+
+function getSpikeDir(){
+  return (width/2) + (p.dirX * (width/2));
+}
+function getSpikeY(index){
+  return (spikeSize/2) + (index * spikeSize);
+}
+
 function generateSpikes(){
+  // areMovingSpikes = false;
+  // movingSpikes = [];
+
   for (let i = 0; i < spikeLength; i++) {
     rectMode(CENTER);
   
-    let dir = (width/2) + (p.dirX * (width/2));
-    let posY = (spikeSize/2) + (i * spikeSize);
+    let dir = getSpikeDir();
+    let posY = getSpikeY(i);
     
+    // areMovingSpikes = spikes.includes(2);
+
     if (spikes[i] == 1){
       rect(dir, posY, spikeSize/2, spikeSize); 
     }
+    // else if (spikes[i] == 2) {
+    //   print("Spike two detected")
+    //   movingSpikes.push({
+    //     x: dir,
+    //     y: posY
+    //   });
+    // }
+    
+    // if (areMovingSpikes){
+    //   // areMovingSpikes = spikes[i] == 2
+    // }
   }
+}
+function areMenerateMovingSpikes(){
+
 }
 function newSpikes() {
   let entry = [];
-  
-  // if (t < 15){
-  entry = spikesEas[floor(random() * spikeLength)];
-  // } else if (t < 30){
-  //   entry = spikesMed[floor(random() * spikeLength)];
-  // } else if (t < 45){
-  //   entry = spikesHar[floor(random() * spikeLength)];
-  // }
+  movingSpikes = [];
+
+  if (t < 15){
+    entry = spikesEas[floor(random() * spikesEas.length)];
+    difficulty = 1;
+  } else if (t < 40){
+    entry = spikesMed[floor(random() * spikesMed.length)];
+    difficulty = 2;
+    // print("selected")
+  } else {
+    entry = spikesHar[floor(random() * spikesHar.length)];
+    difficulty = 3;
+  }
   
   if (random() > 1/2){
     spikes = Array.from(entry.reverse());
   } else {
     spikes = Array.from(entry);
   }
-  // for (let i = 0; i < spikeLength; i++) {
-    // if (random() < spikePer) {
-    //   spikes[i] = 1;
-    // } else {
-    //   spikes[i] = 0;
-    // }
-  // }
+  
+  areMovingSpikes = spikes.includes(2);
+
+  if (areMovingSpikes){
+    for (let i = 0; i < spikeLength; i++) {
+      if (spikes[i] == 2) {
+        print("Spike two detected");
+        
+        let dir = getSpikeDir();
+        let posY = getSpikeY(i);
+
+        let movingSpike = new MovingSpike(posY, dir);
+
+        movingSpikes.push(movingSpike);
+        // movingSpikes.push({
+        //   x: dir,
+        //   y: posY
+        // });
+      }
+    }
+  }
 }
 function gameLoop(){
   t += 1/60;
   textAlign(CENTER);
   fill(0,0,0,0.5*255);
   textSize(60);
-  text(`${score}`, width/2, height/2)
+  text(`${score}`, width/2, height/2);
+  text(`${Math.floor(t)}`, width/2, (height/2)+100)
 }
 function setup() {
   createCanvas(700, 600);
@@ -122,27 +221,89 @@ function setup() {
   p = new Player();
   setSpikes();
 }
+
+
+
 function draw() {
   background(127, 127, 55);
+  rectMode(CENTER);
   gameLoop();
   fill("white");
   generateSpikes();
+
+  if (areMovingSpikes){
+    movingSpikes.forEach((spike) => {
+      spike.update();
+      spike.show();
+    });
+  }
+
+    // spikes.forEach(function(spike){
+    //   // let currentSpikes = getCurrentSpikes();
+
+    //   for (let i = 0; i < spikes.length; i++){
+    //     if (spikes[i] == 2){
+    //       let dir = getSpikeDir();
+    //       let posY = getSpikeY(i);
+
+    //       posY += 100;
+
+    //       rect(dir, posY, spikeSize/2, spikeSize); 
+    //     }
+    //   }
+    // })
+
   p.update();
   p.show();
   if (p.hitWall()) {
     wallHit();
   }
 }
-function checkHitSpikes(pos){
-  if (spikeLength > 0){
-    for (let i = 0; i < spikeLength; i++){
-      if (spikes[i] == 1 && pos > i * spikeSize && pos < (i * spikeSize)+spikeSize){
+function checkHitSpikes(atPosY) {
+  if (spikeLength > 0) {
+    for (let i = 0; i < spikeLength; i++) {
+      // Check for static spikes
+      if (spikes[i] == 1 &&
+        atPosY > i * spikeSize &&
+        atPosY < (i * spikeSize) + spikeSize
+      ) {
         return true;
       }
+
+      // Check for moving spikes
+      if (spikes[i] == 2) {
+        for (let spike of movingSpikes) {
+          // print(spike.y, atPosY, spike.y + spikeSize, atPosY > spike.y && atPosY < spike.y + spikeSize);
+
+          if (
+            atPosY > spike.y &&
+            atPosY < spike.y + spikeSize
+          ) {
+            return true;
+          }
+        }
+      }
     }
-    return false;
   }
+  return false;
 }
+// function checkHitSpikes(pos){
+//   if (spikeLength > 0){
+//     for (let i = 0; i < spikeLength; i++){
+//       if (spikes[i] == 1 && pos > i * spikeSize && pos < (i * spikeSize)+spikeSize){
+//         return true;
+//       }
+//       if (spikes[i] == 2){
+//         movingSpikes.forEach(function(spike){
+//           if (pos > spike.y && pos < spike.y + spikeSize){
+//             return true;
+//           }
+//         })
+//       }
+//     }
+//     return false;
+//   }
+// }
 function wallHit() {
   if (!checkHitSpikes(p.y)){
     p.increaseSpd();
